535ce00b67a5766c0ce905c460c63cfa264887e9
Kaderdev <gest@gmail.com>
2023-09-28 13:00:14 +0000

    correction des buggs

--- user/models.py
+++ user/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Participant(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    number = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    number = models.CharField(max_length=255, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
     
     def __str__(self):
         return self.first_name
